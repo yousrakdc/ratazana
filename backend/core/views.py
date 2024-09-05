4bc6201bf72ffd9fa6a26d1e095e3da666c5b072
yousrakdc <yousrakdc@gmail.com>
2024-09-05 13:47:16 +0200

    added homepage

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from dj_rest_auth.registration.views import RegisterView
 from signup.serializers import CustomRegisterSerializer
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the homepage!")
 
 class CustomRegisterView(RegisterView):
     serializer_class = CustomRegisterSerializer
--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('auth/', include('signup.urls')),
