d422b29525b2f1bba6d39fca6c0aee832968974d
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-09-17 15:03:17 +0200

    Delete frontend directory

--- frontend/src/components/data/sampleJerseys.js
+++
@@ -1,8 +0,0 @@
-const sampleJerseys = [
-    { id: 1, teamName: 'Lakers', price: 99.99, image: '/images/lakers-jersey.jpg' },
-    { id: 2, teamName: 'Warriors', price: 89.99, image: '/images/warriors-jersey.jpg' },
-    // Add more jersey objects here
-  ];
-  
-  export default sampleJerseys;
-  
