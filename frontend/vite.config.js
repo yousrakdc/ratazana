f6782744d5e81242bf0390e0e91c032d5ea4f0dd
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-11-04 15:28:47 +0100

    vite not found

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Fallback to index.html for all routes
-  },
   build: {
-    outDir: 'dist', // Set the correct output directory for production
-    commonjsOptions: {
-      include: [/node_modules/], // Ensure compatibility with commonjs modules
-    },
+    outDir: 'dist',
   },
 });
+
